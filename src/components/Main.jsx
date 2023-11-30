0226fee7b6b3cc242aa342647b03db46edeb0f39
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2023-11-29 22:07:32 -0500

    new commit fix

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,8 +5,6 @@ import Directory from './Directory'
 function Main() {
   return (
     <div>
-      <Header/>
-      <Directory/>
         <img width="1300" src="https://www.honest.com/dw/image/v2/BDBW_PRD/on/demandware.static/-/Library-Sites-HC-content/default/dw1863d39a/2023/homepage/full-width/1023_HolidayBuildYourOwnGift_HDC_Hero_Desktop_NoCopy.jpg?sw=2000 2x, https://www.honest.com/dw/image/v2/BDBW_PRD/on/demandware.static/-/Library-Sites-HC-content/default/dw1863d39a/2023/homepage/full-width/1023_HolidayBuildYourOwnGift_HDC_Hero_Desktop_NoCopy.jpg?sw=1440 1x" alt="Fall 2023 Diapers" title="Fall 2023 Diapers"></img>      
     </div>
   )
